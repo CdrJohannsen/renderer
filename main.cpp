31a7d0361bbafdb7f8f33c6339b2decf4fc3ae60
CdrJohannsen <eisbaer.j.m@gmail.com>
2023-03-22 23:50:35 +0100

    verticies are wrong way...

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <string>
 #include <cmath>
 #include "parseOBJ.cpp"
+#include "vect.cpp"
 #include <iostream>
 using namespace std;
 
@@ -28,59 +29,6 @@ int main()
 
     SDL_SetRenderDrawColor(renderer,255,255,255,255);
     
-    int rx = 90;
-    int ry = 0;
-    int rz = 0;
-    int size = 200;
-    bool running=true;
-    while(running) {
-        while (SDL_PollEvent(&e)) {
-            if(e.type == SDL_QUIT)
-                running = false;
-            else if(e.type == SDL_KEYDOWN)
-            {
-                if(SDLK_d == e.key.keysym.sym) {
-                    ry += 2;
-                }
-                else if(SDLK_a == e.key.keysym.sym) {
-                    ry -= 2;
-                }
-                else if(SDLK_w == e.key.keysym.sym) {
-                    rx += 2;
-                }
-                else if(SDLK_s == e.key.keysym.sym) {
-                    rx -= 2;
-                }
-                else if(SDLK_e == e.key.keysym.sym) {
-                    rz += 2;
-                }
-                else if(SDLK_q == e.key.keysym.sym) {
-                    rz -= 2;
-                }
-                else if(SDLK_ESCAPE == e.key.keysym.sym) {
-                    running=false;
-                }
-                break;
-            }
-        }
-    
-        const std::vector< SDL_Vertex > verts =
-        {
-            { SDL_FPoint{ 100,100 }, SDL_Color{ 255,255,255,255 }, SDL_FPoint{ 0 }, },
-            { SDL_FPoint{ 0,100 }, SDL_Color{ 255,255,255,255 }, SDL_FPoint{ 0 }, },
-            { SDL_FPoint{ 0,0 }, SDL_Color{ 255,255,255,255 }, SDL_FPoint{ 0 }, },
-            { SDL_FPoint{ 100,100 }, SDL_Color{ 255,255,255,255 }, SDL_FPoint{ 0 }, },
-            { SDL_FPoint{ 100,0 }, SDL_Color{ 255,255,255,255 }, SDL_FPoint{ 0 }, },
-            { SDL_FPoint{ 0,0 }, SDL_Color{ 255,255,255,255 }, SDL_FPoint{ 0 }, },
-        };
-
-        SDL_SetRenderDrawColor(renderer,0,0,0,255);
-        SDL_RenderClear(renderer);
-        SDL_RenderGeometry(renderer,nullptr,verts.data(),verts.size(),nullptr,0);
-        SDL_RenderDrawPoint(renderer,960,540);
-
-        SDL_Delay(10);
-    }
     parse();
 
     return 0;
--- vect.cpp
+++ vect.cpp
@@ -7,6 +7,7 @@ class Vect {
     public:
         float x;
         float y;
+        float z;
         Vect operator + (Vect a){return Vect {x+a.x,y+a.y};}
         Vect operator - (Vect a){return Vect {x-a.x,y-a.y};}
         Vect operator * (Vect a){return Vect {x*a.x,y*a.y};}
@@ -19,6 +20,8 @@ class Vect {
         void operator *= (float a) { x *= a;y*=a; }
         void operator /= (Vect a) { x /= a.x;y/=a.y; }
         Vect(float a,float b) {x=a;y=b;}
+        //Vect(float a[]) {x=a[0];y=a[1];z=a[2];}
+        Vect(string a[]) {}
         void reverse() {
             float a=x;
             x=y;
