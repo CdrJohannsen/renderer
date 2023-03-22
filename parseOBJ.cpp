3c9f170a4566f2b82fac8c01051549fa8de28148
CdrJohannsen <eisbaer.j.m@gmail.com>
2023-03-22 23:50:35 +0100

    verticies are wrong way...

--- parseOBJ.cpp
+++ parseOBJ.cpp
@@ -1,26 +1,43 @@
 #include <fstream>
 #include <string>
+#include <vector>
 #include <iostream>
 using namespace std;
+
+void split(string text,string at){
+    vector<string> splitted;
+    size_t pos = 0;
+    string segment;
+    while ((pos = text.find(at)) != string::npos) {
+        segment = text.substr(0, pos);
+        text.erase(0, pos + at.length());
+        splitted.push_back(segment);
+    }
+    splitted.push_back(text);
+    string* d = splitted.data();
+    cout << d[1];
+    return;
+}
 
 void parse(){
     string line;
     string n;
     ifstream model ("cube.obj");
+    vector<string*> v;
     while(!model.eof()){
         getline(model,line);
         if (line[0]=='v'){
             if (line[1]=='n'){continue;}
             line.erase(0,2);
-            size_t pos = 0;
-            string token;
-            string space=" ";
-            while ((pos = line.find(space)) != string::npos) {
-                token = line.substr(0, pos);
-                cout << token << endl;
-                line.erase(0, pos + space.length());
-            }
-            cout << line << endl;
+            //v.push_back(split(line," "));
+            split(line," ");
         }
     }
+    //vector<string> values;
+    //values=*v.data();
+    //for (string i:values){
+    //    cout << i;
+    //}
+    return;
 }
+
