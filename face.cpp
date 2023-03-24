f7faee45c0c346e2d18feb50a334a4968166b642
CdrJohannsen <eisbaer.j.m@gmail.com>
2023-03-24 09:26:31 +0100

    gives a Segmentation fault...

--- face.cpp
+++ face.cpp
@@ -5,20 +5,17 @@ using namespace std;
 
 class Face {
     public:
-        int vert_a;
-        int vert_b;
-        int vert_c;
-        int tex_a;
-        int tex_b;
-        int tex_c;
-        int norm_a;
-        int norm_b;
-        int norm_c;
-
-        Face(int x,int y,int z) {vert_a=x;vert_b=y;vert_c=z;}
+        Vect* vert_a;
+        Vect* vert_b;
+        Vect* vert_c;
+        SDL_FPoint tex_vert;
+        float* norm_a;
+        float* norm_b;
+        float* norm_c;
+        //Face(Vect* x,Vect* y,Vect* z) {vert_a=x;vert_b=y;vert_c=z;}
 };
 
 std::ostream& operator << (std::ostream& out,Face const& a) {
-    out << "1: "<<a.vert_a<<" 2: "<<a.vert_b<< " 3: "<<a.vert_c;
+    out << "1: "<<*a.vert_a<<" 2: "<<*a.vert_b<< " 3: "<<*a.vert_c;
     return out;
 }
