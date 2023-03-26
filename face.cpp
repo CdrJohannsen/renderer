964866eb17f680de43fd25c2138e8fa4707d6f01
CdrJohannsen <eisbaer.j.m@gmail.com>
2023-03-26 01:35:07 +0100

    fixed the segmentation fault

--- face.cpp
+++ face.cpp
@@ -3,19 +3,20 @@
 #include <iostream>
 using namespace std;
 
-struct Face {
+class Face {
     public:
-        Vect* vert_a;
-        Vect* vert_b;
-        Vect* vert_c;
+        Vect vert_a=Vect(0,0,0);
+        Vect vert_b=Vect(0,0,0);
+        Vect vert_c=Vect(0,0,0);
         SDL_FPoint tex_vert;
-        float* norm_a;
-        float* norm_b;
-        float* norm_c;
-        //Face(Vect* x,Vect y,Vect* z) {*vert_a=*x;vert_b=y;*vert_c=*z;}
+        float norm_a;
+        float norm_b;
+        float norm_c;
+        Face() {}
+        Face(Vect a, Vect b, Vect c) {vert_a=a;vert_b=b;vert_c=c;}
 };
 
 std::ostream& operator << (std::ostream& out,Face const& a) {
-    out << "1: "<<*a.vert_a<<" 2: "<<*a.vert_b<< " 3: "<<*a.vert_c;
+    out << "1: "<<a.vert_a<<" 2: "<<a.vert_b<< " 3: "<<a.vert_c;
     return out;
 }
